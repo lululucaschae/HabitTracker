8c45688843343e845fdfd9f42ab98b07a92896a8
Lucas Chae <hi@lucaschae.com>
2022-05-13 12:24:46 +0900

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Lucas Chae on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
